a4d15825bdc1efcd63bc3e4ea20829371829cd10
ffrias9 <framuxs@hotmail.com>
2023-03-21 15:55:39 +0100

    Versión 2.0 - Ahora se pueden ejecutar también workflows.

--- execute_job.py
+++ execute_job.py
@@ -4,23 +4,50 @@ def execute_job(url, user, password, id_job, id_credential):
     auth = (user, password)
 
     url = f"{url}/api/v2/job_templates/{id_job}/launch/"
-
-    exvars = input("¿Desea agregar variables extra? (s/n): ")
-    if exvars == "s":
+    
+    exvars="x"
+    while exvars not in ("s","n","S","N"):
+        exvars = input("¿Desea agregar variables extra? (s/n): ")
+    if exvars in ("s","S"):
         extra_vars = {}
         values = input("Introduzca las variables extras separadas por comas: ")
         elements = values.split(",")
         for element in elements:
             key, value = element.split("=")
-            extra_vars[key] = value
+            extra_vars.update({key: value})
         payload = {"extra_vars": extra_vars, "credentials": [id_credential]}
     else:
         payload = {"extra_vars": {}, "credentials": [id_credential]}
 
-    response = requests.post(url, auth=auth, json=payload)
+    result = requests.post(url, auth=auth, json=payload)
 
-    if response.status_code == 201:
-        job_id = response.json()["job"]
-        return {"status": "success", "job_id": job_id}
+    if result.status_code == 201:
+        return {"status": "success", "job_id": result.json()["job"]}
     else:
-        return {"status": "error", "message": response.text}
+        return {"status": "error", "message": result.text}
+
+def execute_workflow_job(url, user, password, id_job):
+    auth = (user, password)
+
+    url = f"{url}/api/v2/workflow_job_templates/{id_job}/launch/"
+
+    exvars="x"
+    while exvars not in ("s","n","S","N"):
+        exvars = input("¿Desea agregar variables extra? (s/n): ")
+    if exvars in ("s","S"):
+        extra_vars = {}
+        values = input("Introduzca las variables extras separadas por comas: ")
+        elements = values.split(",")
+        for element in elements:
+            key, value = element.split("=")
+            extra_vars.update({key: value})
+        payload = {"extra_vars": extra_vars}
+    else:
+        payload = {"extra_vars": {}}
+
+    result = requests.post(url, auth=auth, json=payload)
+
+    if result.status_code == 201:
+        return {"status": "success", "job_id": result.json()["workflow_job"]}
+    else:
+        return {"status": "error", "message": result.text}
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from execute_job import execute_job
+from execute_job import *
 from getpass import getpass
+from time import sleep
+
+def menu():
+    print("")
+    print("1 - Ejecutar job")
+    print("2 - Ejecutar workflow")
+    print("3 - Salir")
+   
+opcion=0
+while opcion not in range(1,3):
+    menu()
+    opcion = int(input("Seleccione una opción: "))
+    if opcion == 3:
+        quit()
+    if opcion not in (1,2):
+        print("Seleccione una opción correcta [1-3].")
+        sleep(2)
 
 url = "http://192.168.10.50"
 user = input("Introduzca el nombre de usuario: ")
 password = getpass("Introduzca la contraseña: ")
 id_job = input("Introduzca el job id: ")
-id_credential = input("Introduzca el id de las credenciales: ")
 
-output = execute_job(url, user, password, id_job, id_credential)
+if opcion == 1:
+    id_credential = input("Introduzca el id de las credenciales: ")
+    output = execute_job(url, user, password, id_job, id_credential)
+elif opcion == 2:
+    output = execute_workflow_job(url, user, password, id_job)
 
 print(output)
